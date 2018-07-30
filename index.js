af167a67ec1e93c25b35829212d2930687404613
jacksonjude <jacksonjude@mac.com>
2018-07-29 19:27:41 -0700

    Added progress bar, Added more logs

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ function singleSpace(str)
     return str.replace(/\s+/g, " ")
 }
 
+var ProgressBar = require('progress');
+var bar = null
+var barEnabled = false
+
 function getObjectsFromPDF()
 {
-    var promise = new Promise( (resolve, reject) => {
-        var courses = []
-        var blocks = []
+    if (barEnabled)
+        bar = new ProgressBar(':bar', { total: 100 })
 
+    var objectsPromise = new Promise( (resolve, reject) => {
         var PdfReader = require("pdfreader").PdfReader;
         var rawText = Array()
 
@@ -24,7 +28,7 @@ function getObjectsFromPDF()
         var fileURL = "2018F.pdf"
         //var fileURL = "http://lhs-sfusd-ca.schoolloop.com/file/1239686293231/1239686294994/6183066267819394189.pdf"
 
-        console.log("Loading from PDF...")
+        console.log("   Loading from PDF...")
 
         new PdfReader().parseFileItems(fileURL, function(err, item){
             if (item && item.text)
@@ -33,111 +37,141 @@ function getObjectsFromPDF()
             }
             else if (!item)
             {
-                console.log("Loaded rawText!")
+                console.log("   Loaded rawText!")
 
-                var startIndex = null
+                if (barEnabled)
+                    bar.tick(100)
 
-                for (text in rawText)
-                {
-                    if (rawText[text] == "1" && text != 1)
-                    {
-                        startIndex = text
-                        break
-                    }
-                }
+                var objects = getObjectsFromRawText(rawText)
 
-                console.log("Start Index: " + startIndex)
-
-                var tmpArray = []
-                var skipsLeft = 0
-                var departmentNumber = "1"
-                var startedSkipping = false
-
-                for (text in rawText)
-                {
-                    if (rawText[text] == rawText[0])
-                    {
-                        tmpArray = []
-                        skipsLeft = startIndex
-                    }
-
-                    if (skipsLeft > 0)
-                    {
-                        skipsLeft -= 1
-                        continue
-                    }
-
-                    if (tmpArray.length == 0 && parseInt(rawText[text]) != parseInt(departmentNumber) && parseInt(rawText[text])-1 != parseInt(departmentNumber))
-                    {
-                        if (!startedSkipping)
-                        {
-                            startedSkipping = true
-                            courses.pop()
-                            blocks.pop()
-                        }
-                        continue
-                    }
-                    else if (tmpArray.length == 0 && parseInt(rawText[text])-1 == parseInt(departmentNumber) && rawText[text].length == 1)
-                    {
-                        departmentNumber = rawText[text]
-                    }
-
-                    startedSkipping = false
-
-                    tmpArray.push(rawText[text])
-
-                    if (tmpArray.length == 3)
-                    {
-                        var newCourse = new SchoolCourse(tmpArray[0].trim(), tmpArray[1].trim(), singleSpace(tmpArray[2]))
-
-                        var courseExists = false
-                        for (course in courses)
-                        {
-                            if (courses[course].departmentNum == newCourse.departmentNum, courses[course].courseCode == newCourse.courseCode, courses[course].courseName == newCourse.courseName)
-                            {
-                                courseExists = true
-                                break
-                            }
-                        }
-
-                        if (!courseExists)
-                        {
-                            courses.push(newCourse)
-                        }
-                    }
-
-                    if (tmpArray.length == 8)
-                    {
-                        var newBlock = new SchoolBlock(tmpArray[3].trim(), tmpArray[4].trim(), tmpArray[5].trim(), singleSpace(tmpArray[6]), singleSpace(tmpArray[7]), tmpArray[1].trim())
-
-                        blocks.push(newBlock)
-
-                        tmpArray = []
-                    }
-                }
-
-                console.log("Created Objects!")
-
-                resolve([courses, blocks])
+                resolve(objects)
             }
         })
     })
 
-    return promise
+    return objectsPromise
+}
+
+function getObjectsFromRawText(rawText)
+{
+    var courses = []
+    var blocks = []
+
+    var startIndex = null
+
+    for (text in rawText)
+    {
+        if (rawText[text] == "1" && text != 1)
+        {
+            startIndex = text
+            break
+        }
+    }
+
+    console.log("   Start Index: " + startIndex)
+
+    if (barEnabled)
+        bar = new ProgressBar(':bar', { total: rawText.length })
+
+    console.log("   Creating Objects...")
+
+    var tmpArray = []
+    var skipsLeft = 0
+    var departmentNumber = "1"
+    var startedSkipping = false
+
+    for (text in rawText)
+    {
+        if (barEnabled)
+            bar.tick()
+
+        if (rawText[text] == rawText[0])
+        {
+            tmpArray = []
+            skipsLeft = startIndex
+        }
+
+        if (skipsLeft > 0)
+        {
+            skipsLeft -= 1
+            continue
+        }
+
+        if (tmpArray.length == 0 && parseInt(rawText[text]) != parseInt(departmentNumber) && parseInt(rawText[text])-1 != parseInt(departmentNumber))
+        {
+            if (!startedSkipping)
+            {
+                startedSkipping = true
+                courses.pop()
+                blocks.pop()
+            }
+            continue
+        }
+        else if (tmpArray.length == 0 && parseInt(rawText[text])-1 == parseInt(departmentNumber) && rawText[text].length == 1)
+        {
+            departmentNumber = rawText[text]
+        }
+
+        startedSkipping = false
+
+        tmpArray.push(rawText[text])
+
+        if (tmpArray.length == 3)
+        {
+            var newCourse = new SchoolCourse(tmpArray[0].trim(), tmpArray[1].trim(), singleSpace(tmpArray[2]))
+
+            var courseExists = false
+            for (course in courses)
+            {
+                if (courses[course].departmentNum == newCourse.departmentNum, courses[course].courseCode == newCourse.courseCode, courses[course].courseName == newCourse.courseName)
+                {
+                    courseExists = true
+                    break
+                }
+            }
+
+            if (!courseExists)
+            {
+                courses.push(newCourse)
+            }
+        }
+
+        if (tmpArray.length == 8)
+        {
+            var newBlock = new SchoolBlock(tmpArray[3].trim(), tmpArray[4].trim(), tmpArray[5].trim(), singleSpace(tmpArray[6]), singleSpace(tmpArray[7]), tmpArray[1].trim())
+
+            blocks.push(newBlock)
+
+            tmpArray = []
+        }
+    }
+
+    console.log("   Created Objects!")
+
+    return [courses, blocks]
 }
 
 async function loadObjectsToSQL(courses, blocks, completion)
 {
+    if (barEnabled)
+        bar = new ProgressBar(':bar', { total: courses.length + blocks.length })
+
     for (course in courses)
     {
         var coursePromise = courses[course].loadToSQL()
         await coursePromise
+
+        if (barEnabled)
+            bar.tick()
     }
 
     for (block in blocks)
     {
         var blockPromise = blocks[block].loadToSQL()
         await blockPromise
+
+        if (barEnabled)
+            bar.tick()
     }
 
     completion()
@@ -152,10 +186,10 @@ function updateSQLObjects(completion)
         var courses = value[0]
         var blocks = value[1]
 
-        console.log("Loading objects to SQL...")
+        console.log("   Loading objects to SQL...")
 
         loadObjectsToSQL(courses, blocks, function() {
-            console.log("Loaded to SQL!")
+            console.log("   Loaded to SQL!")
             updatingObjects = false
 
             if (completion != null)
@@ -179,11 +213,8 @@ app.get('/query/', function(req, res) {
 
         var connectionPromise = sql.connectWithPromise()
         connectionPromise.then(value => {
-            var sqlString = "select " + req.query.column + " from " + req.query.table
-            if (req.query.key != null && req.query.value != null)
-            {
-                sqlString += " where " + req.query.key + "=" + req.query.value
-            }
+            var sqlString = "select " + (req.query.column ? req.query.column : "*") + " from " + req.query.table + ((req.query.key != null && req.query.value != null) ? " where " + req.query.key + "=\"" + req.query.value + "\"" : "") + (req.query.where ? " where " + req.query.where : "")
+            console.log("GET /query/ => " + sqlString)
 
             con.query(sqlString, function (err, result, fields) {
                 res.json(result)
@@ -194,7 +225,8 @@ app.get('/query/', function(req, res) {
     }
     else
     {
-        res.send("ERROR: Updating objects!")
+        console.log("GET /query/ => ERROR: Updating Objects!")
+        res.status(500).send("ERROR: Updating Objects!")
     }
 })
 
@@ -202,11 +234,13 @@ app.get('/update/', function(req, res) {
     if (!updatingObjects)
     {
         updateSQLObjects(function() {
+            console.log("GET /update/ => OK")
             res.status(200).send("OK")
         })
     }
     else
     {
+        console.log("GET /update/ => ERROR: Already Updating Objects!")
         res.status(500).send("ERROR: Already Updating Objects!")
     }
 })
