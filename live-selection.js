aab0da3b8395133434d84436dbe38286c12019c2
jacksonjude <jacksonjude@mac.com>
2019-05-01 10:25:19 -0700

    Using table parsing code

--- live-selection.js
+++ live-selection.js
@@ -4,6 +4,8 @@
 const fs = require('fs');
 const rp = require('request-promise');
 const tough = require('tough-cookie');
+var cheerio = require('cheerio'),
+    cheerioTableparser = require('cheerio-tableparser');
 
 //const courseSelectionURL = "./pdf/lowellcourseselection.html"
 const courseSelectionURL = "www.lowell-courseselection.org"
@@ -71,7 +73,7 @@ exports.getSeatsForClass = function(className, teacherName, blockNumber, schedul
 exports.getArenaData = function(authCookie) {
   var getArenaDataPromse = new Promise(function(resolve, reject) {
     //var liveSelectionData = fs.readFileSync('./live-selection.json', 'utf8')
-    var liveSelectionData = getCourseSelectionJSON()
+    /*var liveSelectionData = getCourseSelectionJSON()
     if (liveSelectionData != null && Date.now()-JSON.parse(liveSelectionData)["updatedAt"] < 180000 && JSON.parse(liveSelectionData)["data"] != null)
     {
       console.log("parse -- " + liveSelectionData + " -- " + Date.now()-JSON.parse(liveSelectionData)["updatedAt"])
@@ -104,12 +106,41 @@ exports.getArenaData = function(authCookie) {
         writeCourseSelectionJSON(data)
         resolve(data)
       })
+    }*/
 
-      /*fs.readFile(courseSelectionURL, 'utf8', function(err, data) {
-        writeCourseSelectionJSON(data)
-        resolve(data)
-      })*/
-    }
+    /*fs.readFile(courseSelectionURL, 'utf8', function(err, data) {
+      writeCourseSelectionJSON(data)
+      resolve(data)
+    })*/
+
+    let cookie = new tough.Cookie({
+        key: ".ASPXAUTH",
+        value: authCookie,
+        domain: "www.lowell-courseselection.org",
+        httpOnly: true,
+    });
+
+    var cookiejar = rp.jar();
+    cookiejar._jar.rejectPublicSuffixes = false;
+    cookiejar.setCookie(cookie.toString(), 'http://www.lowell-courseselection.org');
+
+    var options = {
+        method: "GET",
+        uri: 'http://www.lowell-courseselection.org/',
+        jar: cookiejar,
+        // resolveWithFullResponse: true
+        transform: function (body) {
+            return cheerio.load(body);
+        }
+    };
+
+    rp(options)
+      .then(function($) {
+        cheerioTableparser($);
+        var table = $("table").parsetable();
+        var newData = table[0].map((col, i) => table.map(row => row[i]));
+        resolve(newData)
+      })
   })
 
   return getArenaDataPromse
